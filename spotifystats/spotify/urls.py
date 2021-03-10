f806523164cd151e253235679b1b58a5e638d891
Paula <aimar493@gmail.com>
2021-03-09 22:51:51 -0300

    changed the way users authenticate to implicit grant

--- spotifystats/spotify/urls.py
+++ spotifystats/spotify/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import AuthURL, spotify_callback
 from . import views
 
 urlpatterns = [
     path('', views.landingPage, name = 'landingpage'),
-    path('get-auth-url/', AuthURL.as_view()),
-    path('redirect/',spotify_callback)
+    path('get-auth-url/', views.getauth),
+    # path('redirect/',views.spotify_callback)
 ]
--- spotifystats/spotify/views.py
+++ spotifystats/spotify/views.py
@@ -1,38 +1,17 @@
 from django.shortcuts import render, redirect
 from decouple import config
-from requests import Request, post
-from django.views.generic.base import View
+from requests import Request
 
-class AuthURL(View):
-    def get(self, request, format=None):
-        scopes = 'user-top-read '
-        Request('GET','https://accounts.spotify.com/authorize', params = {
-            'scope':scopes,
-            'response': 'code',
-            'redirect': config('REDIRECT_URI'),
-            'client_id' : config('SPOTIFY_CLIENT_ID')
-        })
-
-def spotify_callback(request, format=None):
-    code = request.GET.get('code')
-    error = request.GET.get('error')
-
-    response = post('https://accounts.spotify.com/api/token', data = {
-        'grant_type': 'authorization_code',
-        'code' : code,
+def getauth(request):
+    scopes = 'user-top-read '
+    response = Request('GET','https://accounts.spotify.com/authorize', params = {
+        'client_id' : config('SPOTIFY_CLIENT_ID'),
+        'scope':scopes,
+        'response_type': 'token',
         'redirect_uri': config('REDIRECT_URI'),
-        'client_secret': config('SPOTIFY_CLIENT_SECRET')
-
-    }).json()
-
-    access_token = response.get('access_token')
-    token_type = response.get('token')
-    refresh_token =  response.get('refresh_token')
-    expires_in = response.get('expires_in')
-    error = response.get('error')
-
-    return redirect('charts:chartsAndStats')
-
+    }).prepare().url
+    redirect_url = redirect(response)
+    return redirect_url
 
 
 
