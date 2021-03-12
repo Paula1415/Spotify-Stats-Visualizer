0251b61c5d187bc31cf9ba11c12bd50242c8308a
Paula <aimar493@gmail.com>
2021-03-12 20:11:01 -0300

    .

--- spotifystats/spotify/urls.py
+++ spotifystats/spotify/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('', views.landingPage, name = 'landingpage'),
     path('get-auth-url/', views.getauth),
-    path('spotifyredirect', views.spotify_callback),
-    path('userdata/', views.getUserdata)
+    path('Stats/', views.getUserdata)
+
 ]
--- spotifystats/spotify/views.py
+++ spotifystats/spotify/views.py
@@ -2,63 +2,21 @@ from django.shortcuts import render, redirect
 from decouple import config
 from requests import Request, post,get
 from urllib.parse import urlparse
+import tekore as tk
 
 
-
-response = 0
+token = ''
 def getauth(request):
-    global response
-    scopes = 'user-top-read '
-    response = Request('GET','https://accounts.spotify.com/authorize', params = {
-        'client_id' : config('SPOTIFY_CLIENT_ID'),
-        'scope':scopes,
-        'response_type': 'code',
-        'redirect_uri': config('REDIRECT_URI'),
-        }).prepare().url
-    redirect_url = redirect(response)
-    return redirect_url
-
-access_token = 0
-error = 0
-def spotify_callback (request):
-    global access_token
-    global error
-    global response
-
-    code = request.GET.get('code')
-    error = request.GET.get('error')
-
-    if error:
-        context ={'error' : error, 'auth' : response}
-        return render(request, 'onerror.html', context)
-
-    else:
-        response = post('https://accounts.spotify.com/api/token', data={
-            'grant_type': 'authorization_code',
-            'code': code,
-            'redirect_uri': config('REDIRECT_URI'),
-            'client_id': config('SPOTIFY_CLIENT_ID'),
-            'client_secret': config('SPOTIFY_CLIENT_SECRET')
-        }).json()
-
-        access_token = response.get('access_token')
-        token_type = response.get('token_type')
-        refresh_token = response.get('refresh_token')
-        expires_in = response.get('expires_in')
-
-        redirection = redirect('http://127.0.0.1:8000/userdata/')
-        return redirection
+    global token
+    conf = (config('SPOTIFY_CLIENT_ID'), config('SPOTIFY_CLIENT_SECRET'), config('REDIRECT_URI'))
+    token = tk.prompt_for_user_token(*conf, scope=tk.scope.every)
 
 
 def getUserdata(request):
-    global access_token
-
-    usertopreads = get('https://api.spotify.com/v1/me/top/artists', headers ={
-        'Authorization' : 'Bearer',
-        'token' : str(access_token)
-    })
-
-    context = {'userdata' : usertopreads}
+    global token
+    spotify = tk.Spotify(token)
+    tracks = spotify.current_user_top_tracks()
+    context = {'userdata': tracks}
     return render(request, 'userdata.html', context)
 
 def landingPage(request):
